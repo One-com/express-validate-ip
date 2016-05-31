93416704769443e6bb7f2f679539a93fe4a6e94c
Gustav Nikolaj Olsen <gno@one.com>
2016-05-31 11:27:19 +0200

    update tests dependencies (unexpected v10)

--- test/expressValidateIp.js
+++ test/expressValidateIp.js
@@ -12,19 +12,19 @@ describe('expressValidateIp middleware', function () {
 
     var expect = unexpected.clone()
         // Create a custom assertion that mounts the redirectionRules middleware in an Express server and delegates to 'to be middleware that processes'
-        .installPlugin(require('unexpected-express'))
-        .addAssertion('[not] to be allowed access according to', function (expect, subject, value, done) {
-            expect(
+        .use(require('unexpected-express'))
+        .addAssertion('[not] to be allowed access according to', function (expect, subject, value) {
+            expect.errorMode = 'bubble';
+            return expect(
                 express().set('trust proxy', true).use(expressValidateIp({allowedIps: value})),
-                'to be middleware that processes',
+                'to yield exchange',
                 {
                     request: {
                         remoteAddress: subject,
                         url: '/'
                     },
                     response: this.flags.not ? 403 : 404
-                },
-                done
+                }
             );
         });
 
@@ -38,24 +38,24 @@ describe('expressValidateIp middleware', function () {
         }, 'to throw exception', 'validateIp: Cannot parse foobar');
     });
 
-    it('should allow a random IP when allowedIps is set to *', function (done) {
-        expect('99.88.77.66', 'to be allowed access according to', '*', done);
+    it('should allow a random IP when allowedIps is set to *', function () {
+        return expect('99.88.77.66', 'to be allowed access according to', '*');
     });
 
-    it('should disallow a random IP when allowedIps is undefined', function (done) {
-        expect('99.88.77.66', 'not to be allowed access according to', undefined, done);
+    it('should disallow a random IP when allowedIps is undefined', function () {
+        return expect('99.88.77.66', 'not to be allowed access according to', undefined);
     });
 
-    it('should disallow a random IP when allowedIps is []', function (done) {
-        expect('99.88.77.66', 'not to be allowed access according to', undefined, done);
+    it('should disallow a random IP when allowedIps is []', function () {
+        return expect('99.88.77.66', 'not to be allowed access according to', undefined);
     });
 
-    it('should allow a random IP when allowedIps is equal to it', function (done) {
-        expect('99.88.77.66', 'to be allowed access according to', '99.88.77.66', done);
+    it('should allow a random IP when allowedIps is equal to it', function () {
+        return expect('99.88.77.66', 'to be allowed access according to', '99.88.77.66');
     });
 
-    it('should allow a random IP when allowedIps contains it', function (done) {
-        expect('99.88.77.66', 'to be allowed access according to', ['99.88.77.66'], done);
+    it('should allow a random IP when allowedIps contains it', function () {
+        return expect('99.88.77.66', 'to be allowed access according to', ['99.88.77.66']);
     });
 
     var allowedIpsInProduction = [
@@ -65,14 +65,14 @@ describe('expressValidateIp middleware', function () {
     ];
 
     ['195.47.247.51', '91.198.169.133', '10.246.0.1', '10.246.255.255'].forEach(function (whitelistedIp) {
-        it('should allow access from whitelisted ip ' + whitelistedIp, function (done) {
-            expect(whitelistedIp, 'to be allowed access according to', allowedIpsInProduction, done);
+        it('should allow access from whitelisted ip ' + whitelistedIp, function () {
+            return expect(whitelistedIp, 'to be allowed access according to', allowedIpsInProduction);
         });
     });
 
     ['91.198.169.132', '91.198.169.134', '99.88.77.66', '10.245.255.255', '10.247.0.1'].forEach(function (nonWhitelistedIp) {
-        it('should disallow access from non-whitelisted ip ' + nonWhitelistedIp, function (done) {
-            expect(nonWhitelistedIp, 'not to be allowed access according to', allowedIpsInProduction, done);
+        it('should disallow access from non-whitelisted ip ' + nonWhitelistedIp, function () {
+            return expect(nonWhitelistedIp, 'not to be allowed access according to', allowedIpsInProduction);
         });
     });
 });
