a01b508b84087983f8b42b9d652740cbe1956e5f
Gustav Nikolaj Olsen <gno@one.com>
2021-02-24 15:32:26 +0100

    Test the guts of the middleware

--- test/expressValidateIp.js
+++ test/expressValidateIp.js
@@ -2,37 +2,52 @@
 
 process.env.NODE_ENV = 'test'; // Silence Connect's built-in error handler that logs errors to the console
 
-var unexpected = require('unexpected'),
-    os = require('os'),
-    express = require('express'),
-    expressValidateIp = require('../lib/expressValidateIp');
+var expect = require('unexpected').use(require('unexpected-express'));
+var express = require('express');
+var expressValidateIp = require('../lib/expressValidateIp');
 
 describe('expressValidateIp middleware', function () {
-    var expect = unexpected.clone()
-        // Create a custom assertion that mounts the redirectionRules middleware in an Express server and delegates to 'to be middleware that processes'
-        .use(require('unexpected-express'))
-        .addAssertion('[not] to be allowed access according to', function (expect, subject, value) {
-            expect.errorMode = 'bubble';
-            return expect(
-                express().set('trust proxy', true).use(expressValidateIp(value)),
-                'to yield exchange',
-                {
-                    request: {
-                        remoteAddress: subject,
-                        url: '/'
-                    },
-                    response: this.flags.not ? 403 : 404
-                }
-            );
-        });
-
-    it('should throw an exception if passed an invalid allowedIps string', function () {
+    it('should propagate errors from IpWhitelist', function () {
         expect(function () {
             expressValidateIp('foobar');
         }, 'to throw exception', 'validateIp: Cannot parse foobar');
-
-        expect(function () {
-            expressValidateIp(['foobar']);
-        }, 'to throw exception', 'validateIp: Cannot parse foobar');
     });
+
+    it('should not block an allowed ip', function () {
+        return expect(
+            express()
+                .use(expressValidateIp('*'))
+                .use(function (req, res, next) {
+                    res.end('Hello world.');
+                }),
+            'to yield exchange',
+            {
+                request: {
+                    remoteAddress: '1.2.3.4',
+                    url: '/'
+                },
+                response: 200
+            }
+        );
+    });
+
+    it('should block an unallowed ip', function () {
+        return expect(
+            express()
+                .use(expressValidateIp([]))
+                .use(function (req, res, next) {
+                    res.end('Hello world.');
+                }),
+            'to yield exchange',
+            {
+                request: {
+                    remoteAddress: '1.2.3.4',
+                    url: '/'
+                },
+                response: 403
+            }
+        );
+    });
+
+
 });
